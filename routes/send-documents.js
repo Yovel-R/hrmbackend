a055fae03c74f839d4eb948d35ad03cfecb6c3e0
Yovel-R <yovel2911@gmail.com>
2026-03-24 15:49:04 +0530

    mail

--- routes/send-documents.js
+++ routes/send-documents.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const multer = require("multer");
-const nodemailer = require("nodemailer");
+const sendEmail = require("../utilities/sendEmail");
 const router = express.Router();
 
 const upload = multer({ storage: multer.memoryStorage() }); // store files in RAM
@@ -13,15 +13,6 @@ router.post(
     try {
       const { internName, internId } = req.body;
 
-      // Gmail Transporter
-      const transporter = nodemailer.createTransport({
-        service: "gmail",
-        auth: {
-          user: process.env.SENDER_EMAIL_USER,
-          pass: process.env.SENDER_EMAIL_PASS,
-        },
-      });
-
       // Prepare attachments
       const attachments = req.files.map((file) => ({
         filename: file.originalname,
@@ -29,16 +20,13 @@ router.post(
         contentType: "application/pdf",
       }));
 
-      // Email options
-      const mailOptions = {
-        from: process.env.SENDER_EMAIL_USER,
+      // Call our resend utility
+      await sendEmail({
         to: process.env.RECIVER_EMAIL_USER,
         subject: `Intern Documents - ${internName} (${internId})`,
         text: `Hi Admin,\n\nPlease find attached all internship documents for ${internName}.\n\nThanks.`,
         attachments,
-      };
-
-      await transporter.sendMail(mailOptions);
+      });
 
       res.status(200).json({ message: "Email sent successfully!" });
     } catch (err) {
